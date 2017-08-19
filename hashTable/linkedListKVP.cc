633619f114610a598b7cef4bebe21f5204ad1793
David Skudra <dskudra@gmail.com>
2017-08-19 16:46:52 -0400

    Added linkedList structure that holds KVP pairs for hash table.

---
+++ hashTable/linkedListKVP.cc
@@ -0,0 +1,68 @@
+#include <iostream>
+#include "linkedListKVP.h"
+using namespace std;
+
+linkedListKVP::~linkedListKVP () {
+  if (head == NULL) {
+    return;
+  }
+
+  KVPnode * delnode = head;
+  KVPnode * nextnode;
+  while (delnode->next != NULL) {
+    nextnode = delnode->next;
+    delete delnode;
+    delnode = nextnode;
+  }
+  delete delnode;
+}
+
+void linkedListKVP::insertKVP (int newkey, int newvalue) {
+  KVPnode * newnode = new KVPnode(newkey, newvalue);
+  if (head == NULL) {
+    head = newnode;
+  } else {
+    newnode->next = head;
+    head = newnode;
+  }
+}
+
+int linkedListKVP::deleteKVP (int key) {
+  int delval;
+  
+  if (head == NULL) {
+    return -1;
+  }
+
+  if (head->key == key) {
+    delval = head->value;
+    head = head->next;
+    return delval;
+  } else {
+    KVPnode * iternode = head;
+    while (iternode->next != NULL) {
+      if (iternode->next->key == key) {
+        delval = iternode->next->value;
+        iternode->next = iternode->next->next;
+        return delval;
+      }
+      iternode = iternode->next;
+    }
+  return -1;
+  }
+}
+
+int main () {
+  linkedListKVP llist;
+  llist.insertKVP(5,99);
+  llist.insertKVP(12,54);
+  llist.insertKVP(123,19);
+  llist.insertKVP(75, 201);
+  cout << "Deleted key 5, value: " << llist.deleteKVP(5) << endl;
+  cout << "Deleted key 123, value: " <<llist.deleteKVP(123) << endl;
+  cout << "Attempted delete on non-existant key 99, got value: " << llist.deleteKVP(99) << endl;
+  cout << "Deleted key 12, value: " << llist.deleteKVP(12) << endl;
+  cout << "Deleted key 75, value: " << llist.deleteKVP(75) << endl;
+  cout << "Attempted delete on empty llist, got value: " << llist.deleteKVP(12) << endl;
+  return 0;
+}
---
+++ hashTable/linkedListKVP.h
@@ -0,0 +1,23 @@
+struct KVPnode {
+  int key;
+  int value;
+  KVPnode * next;
+
+  KVPnode(int newkey, int newvalue) {
+    key = newkey;
+    value = newvalue;
+    next = NULL;
+  } 
+};
+
+class linkedListKVP {
+private:
+  KVPnode * head;
+public:
+  linkedListKVP() {
+    head = NULL;
+  }
+  ~linkedListKVP();
+  void insertKVP(int newkey, int newvalue);
+  int deleteKVP(int key);
+};
